754a15cf7f4170c268bf6e93215de7877a3d4b2a
charles <codelyfewithcharlie@gmail.com>
2019-10-30 14:10:48 -0700

    updates

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise-middleware";
-import budgetReducer from "./redux/budgetReducer";
+import budgetReducer from "./redux/budget";
 
 const rootReducer = combineReducers({
 	budget: budgetReducer
